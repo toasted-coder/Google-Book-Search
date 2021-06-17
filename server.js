0bc059fbd91f8afd194843236d74a48b635a2e1c
toasted-coder <christmanholmes21@gmail.com>
2021-06-17 09:15:53 -0700

    added server.js file

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+// Connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
